fdfc219de9885e24b07bd09c6b4de5996267a0a3
abovearth <roeland_desmet@hotmail.com>
2017-02-25 14:52:42 +0100

    fixed the opensection shear flow for multicell, probably the code is hard to impossible to understand so if you've got questions about it just ask

--- StressProcessing.py
+++ StressProcessing.py
@@ -17,15 +17,33 @@ def OpenSectionShearFlow(yBarFloor,Sx,Sy,Slice):
     Qyf = Inputs.tsFloor*2*math.sqrt(Inputs.R**2+(Inputs.R-Inputs.hf)**2)*abs(yBarFloor-Slice.yBar)
     BoomAreaTimesX = 0.
     BoomAreaTimesY = 0.
+    for j in xrange(len(Slice.booms)):#upper cell
+        if j<10 or j>25:
+            BoomAreaTimesX += Slice.booms[j].boomArea*abs(Slice.booms[j].x-Slice.xBar)
+            BoomAreaTimesY += Slice.booms[j].boomArea*abs(Slice.booms[j].y-Slice.yBar)
+            if j<10:
+                Slice.booms[j].qb = -((Sx*Slice.Ixx-Sy*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(BoomAreaTimesX) -((Sy*Slice.Iyy-Sx*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(BoomAreaTimesY)
+            elif j>25: #add floor with a corrected orientation
+                Slice.booms[j].qb = -((Sx*Slice.Ixx-Sy*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(-Qxf + BoomAreaTimesX) -((Sy*Slice.Iyy-Sx*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(-Qyf + BoomAreaTimesY)
+            if j==35:
+                Slice.booms[j].qb = 0.#cut
+    BoomAreaTimesX = 0.
+    BoomAreaTimesY = 0.
+    for j in xrange(len(Slice.booms)):#lower cell
+        if j==11:
+            Slice.booms[j].qb = 0. #cut
+        if j>11 and j<25:
+            BoomAreaTimesX += Slice.booms[j].boomArea*abs(Slice.booms[j].x-Slice.xBar)
+            BoomAreaTimesY += Slice.booms[j].boomArea*abs(Slice.booms[j].y-Slice.yBar)
+            if j>11:
+                Slice.booms[j].qb = -((Sx*Slice.Ixx-Sy*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(BoomAreaTimesX) -((Sy*Slice.Iyy-Sx*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(BoomAreaTimesY)
+            if j==24: #add floor
+                Slice.booms[j].qb = -((Sx*Slice.Ixx-Sy*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(Qxf + BoomAreaTimesX) -((Sy*Slice.Iyy-Sx*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(Qyf + BoomAreaTimesY)
     for j in xrange(len(Slice.booms)):
-        BoomAreaTimesX += Slice.booms[j].boomArea*abs(Slice.booms[j].x-Slice.xBar)
-        BoomAreaTimesY += Slice.booms[j].boomArea*abs(Slice.booms[j].y-Slice.yBar)
-    return -((Sx*Slice.Ixx-Sy*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(Qxf + BoomAreaTimesX) -((Sy*Slice.Iyy-Sx*Slice.Ixy)/(Slice.Ixx*Slice.Iyy+Slice.Ixy**2))*(Qyf + BoomAreaTimesY)
-    
+        Slice.booms[j].qb = Slice.booms[j].qb *-1. #correct the orientation from clockwise to counterclockwise
+
 def ClosedSectionShearFlow():
-    #Find the shear centre
     
-    #
     return 0
     
 def TotalShearFlow(qb,qs):
